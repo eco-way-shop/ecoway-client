65b2da1b327584f5fdfe46259e2aceafc7badfd6
kapatsila-mariia <m.kapatsila@gmail.com>
2024-06-23 12:45:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Routes from './Routes';
 import './App.scss';
 import store from './redux/store';
 
-axios.defaults.baseURL = 'http://localhost:3001/api/v1';
+axios.defaults.baseURL = 'https://ecoway-server.onrender.com/';
 axios.defaults.headers.common.Authorization = `Bearer ${localStorage.getItem('CarRentalsToken')}`;
 
 ReactDOM.render(
