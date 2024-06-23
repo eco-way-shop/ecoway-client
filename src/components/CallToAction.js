b571bffbc486eb626a6a7d12fbf3ab0b2eb2ec71
kapatsila-mariia <m.kapatsila@gmail.com>
2024-06-23 16:41:01 +0300

    Update CallToAction.js

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -5,7 +5,7 @@ export default function CallToAction() {
   return (
 
     <section className="call-action">
-      <h4>Обирайте автомобіль, та приходьте на тест-драйв у зручний для вас час</h4>
+      <h4 className="call-action-text">Обирайте автомобіль, та приходьте на тест-драйв у зручний для вас час</h4>
       <p>
         <Link className="btn btn-oval" to="/register">Записатись</Link>
       </p>
