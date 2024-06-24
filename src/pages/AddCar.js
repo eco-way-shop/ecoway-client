a8f489b400b54142ca7ca76e4fe12c963cf40a5b
kapatsila-mariia <m.kapatsila@gmail.com>
2024-06-24 18:48:11 +0300

    Update AddCar.js

--- src/pages/AddCar.js
+++ src/pages/AddCar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import Footer from '../components/Footer';
+import Navigation from '../components/Navigation';
 import AddCarContainer from '../container/AddCarContainer';
 
 export default function AddCar() {
